d8a5de0f31bbf8ad7f78ff0105fc0f9afa6fc05e
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-09-16 21:40:46 +0200

    first work on the comments

---
+++ routes/comment.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const Book = require('../models/book');
+const routeGuard = require('./../middleware/route-guard');
+
+const bookRouter = express.Router();
+
+// to get to page to add a comment
+bookRouter.get('/bookList/:id/edit', routeGuard, (req, res, next) => {
+  const id = req.params.id;
+  Book.findById(id)
+    .then((book) => {
+      res.render('comment-add', { book });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// to create the book comment on a review
+bookRouter.post('/bookList/:id/addcomment', routeGuard, (req, res, next) => {
+  const id = req.params.id;
+  const userId = req.user._id;
+  const { review, bookList } = req.body;
+  Book.findByIdAndUpdate(id, { review, bookList })
+    .then(() => {
+      res.redirect(`/booklist/${userId}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+module.exports = commentRouter;
